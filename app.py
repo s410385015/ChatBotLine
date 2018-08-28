e6842f2e8919f38254230e309e565e0b665eca4a
PumpkinBall <s410385015@gmail.com>
2018-08-28 14:41:21 +0800

    test

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    #message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text='我是胖胖')
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
